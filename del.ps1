c76ddf585716272b9e7d29fe24e3a2dc776d57bb
a67878813 <len888len888@gmail.com>
2020-05-09 16:26:34 +0800

    Create del.ps1

---
+++ del.ps1
@@ -0,0 +1,49 @@
+$myStr='K:\选择删除'
+Set-ExecutionPolicy Unrestricted
+get-childitem $myStr -include "最新地址获取.txt" -recurse |remove-item 
+
+
+get-childitem $myStr -include "*uu*.mp4" -recurse |remove-item 
+
+get-childitem $myStr -include "____*" -recurse |remove-item 
+get-childitem $myStr -include "*.mht" -recurse |remove-item 
+get-childitem $myStr -include "*.url" -recurse |remove-item 
+get-childitem $myStr -include "*.URL" -recurse |remove-item 
+get-childitem $myStr -include "*.gif" -recurse |remove-item 
+get-childitem $myStr -include "*.htm" -recurse |remove-item 
+
+get-childitem $myStr -include "更多福利都在這邊先搶先贏" -recurse |remove-item -recurse  
+get-childitem $myStr -include ".dnd_az!" -recurse |remove-item -recurse 
+get-childitem $myStr -include "論壇文宣" -recurse |remove-item -recurse
+get-childitem $myStr -include "本日新片" -recurse |remove-item -recurse
+get-childitem $myStr -include "更刺激的裸聊*" -recurse |remove-item -recurse
+get-childitem $myStr -include "Sex8.cc宣传" -recurse |remove-item -recurse
+get-childitem $myStr -include "合作宣传" -recurse |remove-item -recurse
+get-childitem $myStr -include "色人党宣传文本" -recurse |remove-item -recurse
+get-childitem $myStr -include "板扎福利网·更多极品，你懂得" -recurse |remove-item -recurse
+get-childitem $myStr -include "Sample" -recurse |remove-item -recurse
+get-childitem $myStr -include "宣傳文檔" -recurse |remove-item -recurse
+
+
+
+
+get-childitem $myStr -include "神级女优-高清无码流出 免费下载_files" -recurse |remove-item -recurse
+get-childitem $myStr -include "*无码流出合集.rar" -recurse |remove-item
+get-childitem $myStr -include "第一会所宣传图.jpg" -recurse |remove-item
+
+get-childitem $myStr -include "*.torrent" -recurse |remove-item
+get-childitem $myStr -include "谜歼种子包.rar" -recurse |remove-item
+get-childitem $myStr -include "萝莉种子包.rar" -recurse |remove-item
+get-childitem $myStr -include "另类剧情种子合集.rar" -recurse |remove-item
+get-childitem $myStr -include "萝莉种子包.rar" -recurse |remove-item
+get-childitem $myStr -include "更多种子图.JPG" -recurse |remove-item
+get-childitem $myStr -include "*快來安裝*.mp4" -recurse |remove-item
+get-childitem $myStr -include "*赚到手软*.mp4" -recurse |remove-item
+
+get-childitem $myStr -include "*免费试看*.mp4" -recurse |remove-item
+get-childitem $myStr -include "*快来下载*.mp4" -recurse |remove-item
+get-childitem $myStr -include "*裸聊直播,*.mp4" -recurse |remove-item
+get-childitem $myStr -include "*美女荷官做爱*" -recurse |remove-item
+get-childitem $myStr -include "*vip1196*.mp4" -recurse |remove-item
+
+#get-childitem 'W:\b' -include "*.baiduyun.downloading" -recurse |remove-item -whatif
